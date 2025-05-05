f0784beba47fc93dd3a152773c5bbdbae6a11094
Bekzhan <rakhimbayevbekzhan@gmail.com>
2025-05-05 09:06:02 +0500

    test mail_client

--- user_service/internal/delivery/grpc_client/mail_client.go
+++ user_service/internal/delivery/grpc_client/mail_client.go
@@ -23,7 +23,7 @@ func NewMailClient(cc *grpc.ClientConn) *MailClient {
 func (m *MailClient) SendConfirmationEmail(email, subject, body string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
+	
 	req := &mailpb.ConfirmationRequest{
 		Email:   email,
 		Subject: subject,
